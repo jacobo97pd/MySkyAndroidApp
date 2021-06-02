90d22a808eafdf3a29cbe49210ae53e71c29b807
jacobo97pd <jacobo97pd@gmail.com>
2021-06-02 19:07:11 +0200

    proyecto mysky app android!

---
+++ app/src/main/java/com/example/myskyapp/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.myskyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Duración en milisegundos que se mostrará el splash
+    private final int DURACION_SPLASH = 1000; // 3 segundos
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Tenemos una plantilla llamada splash.xml donde mostraremos la información que queramos (logotipo, etc.)
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
---
+++ app/src/main/java/com/example/myskyapp/SubidaArchivosActivity.java
@@ -0,0 +1,14 @@
+package com.example.myskyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubidaArchivosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subida_archivos);
+    }
+}
